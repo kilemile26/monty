f24e08118e4316bd663def79618e3cffdea042a0
kilemile26 <harunakilemile@gmail.com>
2023-08-29 12:25:02 -0700

    monty3

--- monty.c
+++ monty.c
@@ -56,6 +56,14 @@ int main(int argc, char *argv[])
 		{
 			swap(&stack, line_number);
 		}
+		else if (strcmp(opcode, "add") == 0)
+		{
+			add(&stack, line_number);
+		}
+		else if (strcmp(opcode, "nop") == 0)
+		{
+			nop(&stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
